ade6061eba1d34fd905bbffdb145e1f5aa9e1583
Lidcer <47243657+Lidcer@users.noreply.github.com>
2020-10-04 18:21:14 +0100

    Add files via upload

---
+++ self_write.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int counter = 0;
+
+void new_function() {
+
+    std::cout << "This code has been self overwritten " << std::to_string(++counter) << std::endl;
+
+}
+
+
+int main() {
+    const std::string FILE_NAME = "self_write.cpp";
+    std::ifstream file(FILE_NAME);
+    std::string line;
+    std::vector<std::string> lines; 
+
+    while(getline(file, line)) {
+        if (line.find("//inject") == 0) {
+                lines.push_back("int counter = 0;");
+                lines.push_back("");
+                lines.push_back("void new_function() {");
+                lines.push_back("");
+                lines.push_back("    std::cout << \"This code has been self overwritten \" << std::to_string(++counter) << std::endl;" );
+                lines.push_back("");
+                lines.push_back("}");
+                lines.push_back("");
+                continue;
+        } else if (line.find("//inject") == 4) {
+                lines.push_back("    new_function();");
+        }
+
+        lines.push_back(line);
+    }
+
+    std::ofstream output (FILE_NAME);
+    int totalLines = lines.size();
+
+    for(std::string l : lines) {
+        output << l << std::endl;
+    }
+    
+    new_function();
+    //inject
+
+    return 0;
+}
